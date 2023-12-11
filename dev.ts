0a6c9e625e16604b019947fe1430f36945209a9f
jackhawks <beichuanalex@gmail.com>
2023-12-11 22:09:15 +0800

    test: cron

--- dev.ts
+++ dev.ts
@@ -1,12 +1,5 @@
-#!/usr/bin/env -S deno run -A --watch=static/,routes/
-
 import dev from "$fresh/dev.ts";
 import config from "./fresh.config.ts";
-
 import "$std/dotenv/load.ts";
-
-Deno.cron("sample cron", "*/3 * * * *", () => {
-  console.log("cron job executed every 10 minutes");
-});
 
 await dev(import.meta.url, "./main.ts", config);
