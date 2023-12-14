3c72ef6f565824337bcb229b2e0967c1077c2482
jackhawks <beichuanalex@gmail.com>
2023-12-15 03:47:53 +0800

    feat: add remote ip page

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,13 @@
 import { PageProps } from "$fresh/server.ts";
 
 export default function App({ Component }: PageProps) {
+  const html_title = Deno.env.get("HTML_TITLE");
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>ip2</title>
+        <title>{html_title}</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
