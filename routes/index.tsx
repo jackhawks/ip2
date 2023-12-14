f9ddf6b3a6d9078be86b72669763012ec52a4f9b
jackhawks <beichuanalex@gmail.com>
2023-12-15 04:25:27 +0800

    feat: *test real client ip

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,16 +1,42 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 
 interface Data {
-  remoteIp: string;
+  ip1: string | null;
+  ip2: string | null;
+  ip3: string | null;
+  ip4: string | null;
+  ip5: string | null;
+  ip6: string;
 }
 
 export const handler: Handlers<Data> = {
   GET(req, ctx) {
-    const remoteIp = ctx.remoteAddr.hostname;
-    return ctx.render({ remoteIp });
+    const ip1 = req.headers.get("X-Forwarded-For");
+    const ip2 = req.headers.get("Forwarded");
+    const ip3 = req.headers.get("X-Real-IP");
+    const ip4 = req.headers.get("CF-Connecting-IP");
+    const ip5 = req.headers.get("True-Client-IP");
+    const ip6 = ctx.remoteAddr.hostname;
+    return ctx.render({
+      ip1,
+      ip2,
+      ip3,
+      ip4,
+      ip5,
+      ip6,
+    });
   },
 };
 
 export default function MyPage(props: PageProps<Data>) {
-  return <p>you ip is: {props.data.remoteIp}</p>;
+  return (
+    <>
+      <p>you ip is1: {props.data.ip1}</p>
+      <p>you ip is2: {props.data.ip2}</p>
+      <p>you ip is3: {props.data.ip3}</p>
+      <p>you ip is4: {props.data.ip4}</p>
+      <p>you ip is5: {props.data.ip5}</p>
+      <p>you ip is6: {props.data.ip6}</p>
+    </>
+  );
 }
