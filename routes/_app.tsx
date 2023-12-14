3ed41a4e2fdb024c8733420855792c3f18694aa9
jackhawks <beichuanalex@gmail.com>
2023-12-15 03:55:27 +0800

    fix: env variable file value error

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,13 +1,12 @@
 import { PageProps } from "$fresh/server.ts";
 
 export default function App({ Component }: PageProps) {
-  const html_title = Deno.env.get("HTML_TITLE");
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{html_title}</title>
+        <title>ip2</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
